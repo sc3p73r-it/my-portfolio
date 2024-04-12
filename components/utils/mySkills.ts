58cfae8cecf2501c4add8f578662e241d57e7a87
sc3p73r-it <sc3p73r@outlook.com>
2024-04-12 12:38:33 +0630

    skill add

--- components/utils/mySkills.ts
+++ components/utils/mySkills.ts
@@ -5,6 +5,10 @@ export const skills = [
     techs: ['Windows', 'Windows Server', 'Ubuntu', 'CentOS', 'Red Hat', 'Rocky Linux']
   },
   {
+    title: 'Virtualization',
+    techs: ['VMware ESXi','Proxmox']
+    },
+  {
     title: 'Open-Source Softwares and tools',
     techs: ['Snipe-IT', 'osTicket', 'Git', 'GitHub', 'OpenProject', 'Odoo' ]
   },
