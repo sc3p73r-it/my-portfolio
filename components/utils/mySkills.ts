bb2e5e97f2a2381549e0e1a2de01d19ea69d11bb
sc3p73r-it <sc3p73r@outlook.com>
2024-04-12 12:29:10 +0630

    skill add

--- components/utils/mySkills.ts
+++ components/utils/mySkills.ts
@@ -5,27 +5,11 @@ export const skills = [
     techs: ['Windows', 'Windows Server', 'Ubuntu', 'CentOS', 'Red Hat', 'Rocky Linux']
   },
   {
-    title: 'JavaScript libraries',
-    techs: ['Redux', 'TanStack Query', 'Zustand', 'Formik', 'Jest', 'Storybook']
-  },
-  {
-    title: 'Preprocessors, css style, component and motion libraries',
-    techs: ['Pug', 'Scss', 'Sass', 'Less', 'Bootstrap', 'Tailwind', 'ChakraUI', 'MaterialUI', 'NextUI', 'Framer Motion']
-  },
-  {
-    title: 'Backend stack',
-    techs: ['noSQL', 'Node.js', 'Express.js', 'MongoDB', 'Postman', 'Firebase', 'Sanity CMS']
-  },
-  {
-    title: 'Softwares and tools',
-    techs: ['VS Code', 'Jira', 'Git', 'GitHub', 'Bitbucket', 'ESLint', 'Husky', 'Figma', 'Photoshop', 'WordPress', 'Elementor']
-  },
-  {
-    title: 'Agile methodologies and coding principles',
-    techs: ['GitFlow', 'SCRUM', 'Clean Code', 'SOLID', 'Clean Architecture', 'Pixel perfect']
+    title: 'Open-Source Softwares and tools',
+    techs: ['VS Code', 'osTicket', 'Git', 'GitHub', 'OpenProject', 'Odoo' ]
   },
   {
     title: 'Currently or interest in learning...',
-    techs: ['SQL', 'Nest', 'GraphQL', 'PostgreSQL', 'Socket.IO', 'Linux', 'AWS', 'Docker', 'Kubernetes', 'CI/CD', 'Terraform', 'Jenkins']
+    techs: ['AWS', 'Azure', 'Docker', 'Kubernetes', 'CI/CD', 'Terraform', 'Jenkins']
   }
 ];
