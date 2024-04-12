4d4ad822a8c4067a52ccaae64ab58cc0a1de252e
sc3p73r-it <sc3p73r@outlook.com>
2024-04-12 15:46:56 +0630

    blogsection title add

--- app/(user)/blog/page.tsx
+++ app/(user)/blog/page.tsx
@@ -6,7 +6,7 @@ const Blog = () => (
 
     <div className='w-full flex flex-col items-center gap-6' style={{ height: '100vh' }}>
 
-      <TitleSectionPageContainer title='I have no idea how to write typescript language :3' />
+      <TitleSectionPageContainer title='I have no idea how to write BlogSection in typescript language! 😂' />
 
     </div>
 
